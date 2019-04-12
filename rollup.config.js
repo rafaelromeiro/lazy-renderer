2a0a8492b66a1f8e851f38650bdc0487df6d9dea
Rafael Oliveira Romeiro <Rafael.Oliveira@ibm.com>
2019-04-12 14:43:09 -0300

    Added IIFE version

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,18 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
+function toPascalCase (name) {
+    return name.match(/[a-z]+/gi).map(word => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()).join('');
+}
+
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+const globals = external.reduce((globals, name) => Object.assign(globals, {[name]: toPascalCase(name)}), {});
 
 export default {
     input: './src/index.ts',
-
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-
+    external,
     plugins: [resolve({ extensions }), commonjs(), babel({ extensions, include: ['src/**/*'] })],
-
     output: [
         {
             file: pkg.main,
@@ -20,6 +23,12 @@ export default {
         {
             file: pkg.module,
             format: 'esm',
+        },
+        {
+            file: pkg.browser,
+            format: 'iife',
+            name: toPascalCase(pkg.name),
+            globals,
         },
     ],
 };
